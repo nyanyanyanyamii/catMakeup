77258f1eabc45cae117ee80cb92e374ee5d93f76
nyanyanyanyamii <116897025+nyanyanyanyamii@users.noreply.github.com>
2023-09-17 21:08:45 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -15,9 +15,14 @@ release = '1.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    'recommonmark',
     'sphinx_markdown_tables'
 ]
-
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -25,7 +30,7 @@ language = 'zh_CN'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+import sphinx_rtd_theme
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 
